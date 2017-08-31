9cc90c91dc18cf992bdafaf83a6c952f1ef1a718
Blaine Huey <bhthenexttesla@gmail.com>
2017-08-31 17:34:02 -0400

    Set Up Basic UI and Response Framework

--- app/src/main/java/independent_study/lightningtimer/MainActivity.java
+++ app/src/main/java/independent_study/lightningtimer/MainActivity.java
@@ -1,13 +1,63 @@
 package independent_study.lightningtimer;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private TextView timeTextView;
+    private Button startStopButton;
+    private Handler handler;
+
+    private boolean buttonState;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("Lightning Timer", "Main Activity onCreate()");
+
+        timeTextView = (TextView) findViewById(R.id.TimerTextView);
+        startStopButton = (Button) findViewById(R.id.StartStopButton);
+        handler = new Handler();
+
+        buttonState = false;
+
+        startStopButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if(!buttonState)
+                {
+                    buttonState = true;
+                    startStopButton.setText("Stop");
+                    activateTimer();
+                }
+                else
+                {
+                    buttonState = false;
+                    startStopButton.setText("Start");
+                    deactivateTimer();
+                }
+            }
+        });
+    }
+
+    private void activateTimer()
+    {
+
+    }
+
+    private void deactivateTimer()
+    {
+        
     }
 }
