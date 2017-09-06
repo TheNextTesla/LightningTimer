bb7e7ec1cf926f232ac20543995cd8680877be4e
Blaine Huey <bhthenexttesla@gmail.com>
2017-09-06 13:05:50 -0400

    Small UI Changes and Code Cleanliness

--- app/src/main/java/independent_study/lightningtimer/MainActivity.java
+++ app/src/main/java/independent_study/lightningtimer/MainActivity.java
@@ -8,13 +8,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+/**
+ * Created by Blaine Huey
+ */
+
 public class MainActivity extends AppCompatActivity
 {
     private TextView timeTextView;
     private Button startStopButton;
     private Handler handler;
+    private Runnable runnable;
 
     private boolean buttonState;
+    private long timerCount;
+    private long startCount;
+    private long lastCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -28,7 +36,22 @@ public class MainActivity extends AppCompatActivity
         startStopButton = (Button) findViewById(R.id.StartStopButton);
         handler = new Handler();
 
+        runnable = new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                lastCount = System.currentTimeMillis();
+                timerCount = lastCount - startCount;
+                timeTextView.setText(Long.toString(timerCount));
+                handler.postDelayed(this, 0);
+            }
+        };
+
         buttonState = false;
+        timerCount = 0L;
+        startCount = System.currentTimeMillis();
+        lastCount = startCount;
 
         startStopButton.setOnClickListener(new View.OnClickListener()
         {
@@ -38,13 +61,13 @@ public class MainActivity extends AppCompatActivity
                 if(!buttonState)
                 {
                     buttonState = true;
-                    startStopButton.setText("Stop");
+                    startStopButton.setText("Stop Timer");
                     activateTimer();
                 }
                 else
                 {
                     buttonState = false;
-                    startStopButton.setText("Start");
+                    startStopButton.setText("Start Timer");
                     deactivateTimer();
                 }
             }
@@ -53,11 +76,18 @@ public class MainActivity extends AppCompatActivity
 
     private void activateTimer()
     {
+        Log.d("Lightning Timer", "Button Activated");
 
+        timerCount = 0;
+        startCount = System.currentTimeMillis();
+
+        handler.removeCallbacks(runnable);
+        handler.postDelayed(runnable, 0);
     }
 
     private void deactivateTimer()
     {
-        
+        Log.d("Lightning Timer", "Button De-Activated");
+        handler.removeCallbacks(runnable);
     }
 }
