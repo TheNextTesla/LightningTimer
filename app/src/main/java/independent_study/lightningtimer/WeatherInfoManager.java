ebd1bee9841521785c5654dd3eb4994261901503
Blaine Huey <bhthenexttesla@gmail.com>
2017-09-12 13:34:01 -0400

    Code Clean, Continued

--- app/src/main/java/independent_study/lightningtimer/WeatherInfoManager.java
+++ app/src/main/java/independent_study/lightningtimer/WeatherInfoManager.java
@@ -14,13 +14,13 @@ import java.util.Locale;
 /**
  * Created by Blaine Huey on 9/8/2017.
  * Gathers and Updates Current Weather Information
+ * @see "http://openweathermap.org/current"
  */
 
 class WeatherInfoManager
 {
-    //http://openweathermap.org/current
-    private String OPENWEATHERAPI_COORDINATE_FORMAT;
-    private String OPENWEATHERAPI_ZIPCODE_FORMAT;
+    private String openweatherapiCoordinateFormat;
+    private String openweatherapiZipcodeFormat;
     private boolean internetTemperature;
     private boolean locationServices;
     private double defaultTemp;
@@ -33,18 +33,24 @@ class WeatherInfoManager
 
     WeatherInfoManager(SharedPreferences sharedPreferences, LocationManager locationManagerEx, String apiKey)
     {
+        //Gathers Information About User Settings
         internetTemperature = sharedPreferences.getBoolean("internet_switch", false);
         locationServices = sharedPreferences.getBoolean("location_switch", false);
         defaultTemp = Utilities.convertTemperature(Double.parseDouble(sharedPreferences.getString("default_temperature_text", "75")), Utilities.TEMPERATURE_CONVERT.FAHRENHEIT_TO_KELVIN);
         defaultZip = sharedPreferences.getString("default_location_text", "19355");
 
-        OPENWEATHERAPI_COORDINATE_FORMAT = "https://api.openweathermap.org/data/2.5/weather?lat=%5f&lon=%5f&appid=" + apiKey;
-        OPENWEATHERAPI_ZIPCODE_FORMAT = "https://api.openweathermap.org/data/2.5/weather?zip=%5s,us&appid=" + apiKey;
+        //The Link that Will Pass Back Current Weather Conditions Given Coordinates or zip-code Respectively
+        openweatherapiCoordinateFormat = "https://api.openweathermap.org/data/2.5/weather?lat=%5f&lon=%5f&appid=" + apiKey;
+        openweatherapiZipcodeFormat = "https://api.openweathermap.org/data/2.5/weather?zip=%5s,us&appid=" + apiKey;
 
         locationManager = locationManagerEx;
+
+        //Criterion for How to Find Location
+        //https://developer.android.com/reference/android/location/Criteria.html
         Criteria criteria = new Criteria();
         criteria.setAccuracy(Criteria.ACCURACY_COARSE);
 
+        //The Thread that Gathers Temperature Data if Internet is Online
         internetUpdater = new Thread()
         {
             @Override
@@ -57,8 +63,10 @@ class WeatherInfoManager
 
                     if(locationServices && internetTemperature)
                     {
+                        //Tries to Get Location
                         try
                         {
+                            //https://stackoverflow.com/questions/20438627/getlastknownlocation-returns-null
                             location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
                         }
                         catch(SecurityException se)
@@ -66,6 +74,7 @@ class WeatherInfoManager
                             se.printStackTrace();
                         }
 
+                        //Checks Whether Location Attempt Worked
                         if(location != null)
                         {
                             Log.d("Weather Info Manager", "Changed Location to be " + location.getLatitude() + " " + location.getLongitude());
@@ -74,15 +83,21 @@ class WeatherInfoManager
                         }
                         else
                         {
+                            //If Location Attempt Fails, Run the Same Code as If Location Were Off (See Below)
                             defaultTemp = getCurrentTemperature(defaultZip);
                             Log.d("Weather Info Manager", "Changed (ZIP) Temperature to be " + defaultTemp + " K");
                         }
                     }
                     else if(internetTemperature)
                     {
+                        //Code to Run Using Default Zip Code
                         defaultTemp = getCurrentTemperature(defaultZip);
+                        Log.d("Weather Info Manager", "Changed (ZIP) Temperature to be " + defaultTemp + " K");
                     }
 
+                    //No Else Necessary..., It will Just Use the Original Default Temperature
+
+                    //Wait a Few Seconds
                     try
                     {
                         Thread.sleep(5000);
@@ -95,27 +110,42 @@ class WeatherInfoManager
             }
         };
 
+        //Tell the Internet Updater to Start the run
         internetUpdater.start();
     }
 
-    public double getCurrentSpeedOfSound()
+    /**
+     * Returns An Estimate of the Speed of Sound (M/s) based on defaultTemp
+     * @return An Estimate of the Speed of Sound (M/s) based on defaultTemp
+     */
+    double getCurrentSpeedOfSound()
     {
         return Utilities.speedOfSoundAtTemperature(defaultTemp);
     }
 
-    public double getBestTemperatureEstimate()
+    /**
+     * Returns Default Temp
+     * @return Default Temp
+     */
+    double getBestTemperatureEstimate()
     {
         return defaultTemp;
     }
 
-    public double getCurrentTemperature(final double lat, final double lon)
+    /**
+     * Calls the Internet OpenWeatherAPI for Temperature (K) based on Lat and Lon
+     * @param lat - Latitude
+     * @param lon - Longitude
+     * @return Temperature at Location in K
+     */
+    double getCurrentTemperature(final double lat, final double lon)
     {
         apiCall = null;
         double tempKelvin = Double.NaN;
 
         try
         {
-            apiCall = Utilities.getFromHTML(String.format(Locale.US, OPENWEATHERAPI_COORDINATE_FORMAT, lat, lon));
+            apiCall = Utilities.getFromHTML(String.format(Locale.US, openweatherapiCoordinateFormat, lat, lon));
         }
         catch (Exception ex)
         {
@@ -138,14 +168,19 @@ class WeatherInfoManager
         return tempKelvin;
     }
 
-    public double getCurrentTemperature(String zipCode)
+    /**
+     * Calls the Internet OpenWeatherAPI for Temperature (K) based on Zip Code
+     * @param zipCode - US Zip Code
+     * @return Temperature at Location in K
+     */
+    double getCurrentTemperature(String zipCode)
     {
         apiCall = null;
         double tempKelvin = Double.NaN;
 
         try
         {
-            apiCall = Utilities.getFromHTML(String.format(Locale.US, OPENWEATHERAPI_ZIPCODE_FORMAT, zipCode));
+            apiCall = Utilities.getFromHTML(String.format(Locale.US, openweatherapiZipcodeFormat, zipCode));
         }
         catch (Exception ex)
         {
